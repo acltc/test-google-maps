3659ae0f4347b23123877c645a62b2983f3c3280
Peter Jang <peter.x.jang@gmail.com>
2016-04-25 14:02:25 -0500

    alternate code to pin array of addresses with info windows

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -9,21 +9,7 @@ function initMap() {
     {lat: -25.363, lng: 131.044, description: "A place in Australia"},
     {lat: -33.8675, lng: 151.2070, description: "The main city, stuff happens here!"}
   ];
-  places.forEach(function(place) {
-    var infowindow = new google.maps.InfoWindow({
-      content: place.description
-    });
-
-    var marker = new google.maps.Marker({
-      position: {lat: place.lat, lng: place.lng},
-      map: map
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-  });
-  // for (var i = 0; i < places.length; i++) {
-  //   var place = places[i];
+  // places.forEach(function(place) {
   //   var infowindow = new google.maps.InfoWindow({
   //     content: place.description
   //   });
@@ -35,5 +21,22 @@ function initMap() {
   //   marker.addListener('click', function() {
   //     infowindow.open(map, marker);
   //   });
-  // }
+  // });
+  for (var i = 0; i < places.length; i++) {
+    addMarker(places[i]);
+  }
+
+  function addMarker(place) {
+    var infowindow = new google.maps.InfoWindow({
+      content: place.description
+    });
+
+    var marker = new google.maps.Marker({
+      position: {lat: place.lat, lng: place.lng},
+      map: map
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+  }
 }
